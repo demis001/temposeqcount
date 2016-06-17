1efd3d49010377b907d89cafe93de1c1d29b7e50
Dereje Jima <ddjima2014@gmail.com>
2016-06-17 12:31:06 -0400

    added tasks  for fastq without barcodes #7

--- raslpipe/tasks.py
+++ raslpipe/tasks.py
@@ -9,14 +9,10 @@ from helpers import get_options, make_logger, runCommand,which,run_cmd
 import os.path
 from pkg_resources import resource_filename, resource_stream
 from pandas import DataFrame
-from rpy2 import robjects
-import rpy2
-from rpy2.robjects import pandas2ri
-from rpy2.robjects.lib import ggplot2
-from rpy2.robjects.packages import importr
+import pandas as pd
 from tabulate import tabulate
-grdevices = importr('grDevices')
-pandas2ri.activate()
+import pandas as pd
+from pandas import DataFrame
 
 
 def work_dir(dirname):
@@ -67,19 +63,37 @@ def createQuality(input, output):
     return
 
 
-#ProbeName   AcceptorProbeSequence   DonorProbeSequence  AcceptorAdaptorSequence DonorAdaptorSequence    seq seq_len
-#NM_000569.6_FCGR3A_2014 CAATGAACAAAGCTACACAG    GAATTAGATATTGAAGCAGA    GGAAGCCTTGGCTTTTG   AGATCGGAAGAGCACAC   CAATGAACAAAGCTACACAGGAATTAGATATTGAAGCAGA    40
 def db_file(input,output):
     with open(output, 'w') as out, open(input, 'r') as infile:
         for line in infile:
-            if line.startswith("ProbeName"):
+            if line.startswith("Probe"):
                 next
             else:
                 line = line.strip()
-                probName,accProbSeq, donProbSeq, accAdapSeq, donAdapSeq,seq,seqLen = line.split("\t")
-                newline = ">" + probName + "\n" + seq + "\n"
+                print line
+                probe_ID, Attenuation_Correction_Factor,Gene_Symbol,Probe_Sequence,Main_RefSeq,RefSeqs_Targeted,Distance_from_5_Prime_End_of_mRNA = line.split(",")
+                newline = ">" + probe_ID + "\n" + Probe_Sequence + "\n"
                 out.write(newline)
     return
+
+def create_gtf(input, output):
+    """Creating custom gtf file from the manifest file"""
+    with open(output, 'w') as out, open(input, 'r') as infile:
+        for line in infile:
+            if line.startswith("Probe"):
+                next
+            else:
+                line = line.strip()
+                probe_ID, Attenuation_Correction_Factor,Gene_Symbol,Probe_Sequence,Main_RefSeq,RefSeqs_Targeted,Distance_from_5_Prime_End_of_mRNA = line.split(",")
+                seqLen = len(Probe_Sequence)
+                gene = probe_ID +"\tprocessed_transcript\t"+ "gene\t"+   str(1) +"\t" +str(seqLen) + "\t.\t.\t.\t" + "gene_id " +'"' + probe_ID+'"; '+"gene_name"+' "'+Gene_Symbol+'";\n'
+                transcript = probe_ID +"\tprocessed_transcript\t"+ "transcript\t"  + str(1) +"\t" +str(seqLen) + "\t.\t.\t.\t" + "gene_id " +'"' + probe_ID+'"; '+"transcript_id"+' "'+probe_ID+'";'  + "gene_name " + '"'+Gene_Symbol+'";\n'
+                exon = probe_ID +"\tprocessed_transcript\t"+ "exon\t"+  str(1) +"\t" +str(seqLen) + "\t.\t.\t.\t" + "gene_id "+ '"' + probe_ID+'"; '+"transcript_id"+' "'+probe_ID+'"; '+'exon_number "1"; ' + "gene_name " + '"'+Gene_Symbol+'";\n'
+                out.write(gene)
+                out.write(transcript)
+                out.write(exon)
+    return
+
 
 def index_db_file(input, output, cpuNum):
     """Index the probe fastfile to use as db file"""
@@ -104,15 +118,14 @@ def prepare_barcode_file(barcodeFile, outFile):
                 next
             else:
                 plateBarcode, wallBarcode, con = line.split()
-                platLine = "PL_" + plateBarcode  + "\t" + plateBarcode + "\n"
+                platLine = "PL__" + plateBarcode  + "\t" + plateBarcode + "\n"
                 plate.write(platLine)
-                wallLine = "WA_" + wallBarcode + "\t" + wallBarcode + "\n"
+                wallLine = "WA__" + wallBarcode + "\t" + wallBarcode + "\n"
                 wall.write(wallLine)
 
 def split_plate_barcode(barcodes,fastqFile, outDir, prefixPlate,suffix):
     """split fastq based on plate barcode"""
     platebc = barcodes[0]
-    #wallbc = barcodes[1]
     cmds = [
         'cat', fastqFile, '|',
         'fastx_barcode_splitter.pl',
@@ -131,7 +144,6 @@ def split_plate_barcode(barcodes,fastqFile, outDir, prefixPlate,suffix):
 
 def split_well_barcode(wellbarcode, fastqFile, inputDir, prefixWell, suffix):
     """split fastq based on plate barcode"""
-    #wallbc = barcodes[1]
     cmds = [
         'cat', fastqFile, '|',
         'fastx_barcode_splitter.pl',
@@ -146,77 +158,6 @@ def split_well_barcode(wellbarcode, fastqFile, inputDir, prefixWell, suffix):
     cmds ='  '.join(cmds)
     cmds +=" 2>&1 | tee   " + welllog
     runCommand(cmds, True)
-    return
-    # Combine wall Barcode splitted files
-
-def plot_summary(logWall,directory,expt_id_wall):
-    #TODO: The lebel of reversed, fix that in a plot
-
-    #with open(logPlate, 'r') as inFile:
-        #barcodes = []
-        #counts = []
-        #matches = []
-        #for line in inFile:
-            #line.strip()
-            #if line.startswith("Barcode") or line.startswith("total"):
-                #next
-            #else:
-                #barcode, count, match = line.split()
-                #barcodes.append(barcode)
-                #counts.append(count)
-                #matches.append(match)
-
-    #df = DataFrame({'barcode': barcodes,
-                    #'count': counts,
-                    #'matched': matches})
-    #df = df.ix[:, 0:2]
-    #p = ggplot2.ggplot(df) + \
-        #ggplot2.aes_string(x='factor(barcode)', y='count') + \
-        #ggplot2.geom_bar(position="dodge",stat="identity") + \
-        #ggplot2.geom_jitter() + ggplot2.coord_flip() + \
-        #ggplot2.ggtitle(label = expt_id_plate) + \
-        #ggplot2.ggplot2.xlab(label = "") +  ggplot2.theme_bw()
-        ##ggplot2.scale_y_continuous(name = "Count\n(million reads)")
-
-    #filename = "{0}/{1}.png".format(directory, expt_id_plate)
-    #print filename
-    #grdevices.png(file=filename, width=4, height=5, unit='in', res=300)
-    #p.plot()
-    #grdevices.dev_off()
-    with open(logWall, 'r') as inFile:
-        plates = []
-        barcodes = []
-        counts = []
-        matches = []
-        for line in inFile:
-            line.strip()
-            if line.startswith("Plate"):
-                next
-            else:
-                plate, well, count, match = line.split("\t")
-                plates.append(plate)
-                barcodes.append(well)
-                counts.append(count)
-                matches.append(match)
-
-    df = DataFrame({'platebarcode':plates,
-                    'wellbarcode': barcodes,
-                    'count': counts,
-                    'matched': matches})
-    df = df.ix[:, 0:4]
-    print df
-    p = ggplot2.ggplot(df) + \
-        ggplot2.aes_string(x='wellbarcode', y='count') + \
-        ggplot2.geom_bar(position="dodge",stat="identity") + \
-        ggplot2.coord_flip() + \
-        ggplot2.ggtitle(label = expt_id_wall) + \
-        ggplot2.ggplot2.xlab(label = "") +  ggplot2.theme_bw()
-        #ggplot2.scale_y_continuous(name = "Count\n(million reads)")
-    filename = "{0}".format(expt_id_wall)
-    print filename
-    grdevices.png(file=filename, width=4, height=5, unit='in', res=300)
-    p.plot()
-    grdevices.dev_off()
     return
 
 def trim_Ad1_Ad2(input_fq, trimleft, trimright, output_fq):
@@ -236,7 +177,7 @@ def combine_wellsplit_logfiles(input_file_names,output_file):
     with open(output_file, "w") as oo:
         oo.write("Plate_Barcode\tWell_Barcode\tCount\tLocation\n")
         for input_file in input_file_names:
-            infile=input_file.split("_")
+            infile=input_file.split("__")
             plateBarcode = infile[3]
             print "   Recombine %s -> %s " %(input_file, output_file)
             for line in open(input_file):
@@ -249,22 +190,175 @@ def combine_wellsplit_logfiles(input_file_names,output_file):
     return
 
 
-def map_seq_to_probes(fastq, genomeDir, numCPU, outPrefix):
+def map_seq_to_probes(fastq, genomeDir, numCPU, outPrefix, gtfFile):
     """Map the sequence to the probes genome file using STAR"""
     cmds = [
         'STAR',
         '--genomeDir', genomeDir,
         '--readFilesIn', fastq,
+        '--readFilesCommand zcat',
         '--runThreadN ', str(numCPU),
         '--outFileNamePrefix', outPrefix,
-        '--twopassMode Basic',
+        '--outSAMtype BAM SortedByCoordinate',
+        '--quantMode GeneCounts',
+        '--sjdbGTFfile', gtfFile,
         ' --genomeLoad NoSharedMemory',
     ]
     cmds = '  '.join(cmds)
     runCommand(cmds, True)
 
+def convertSamToBam(samfile, bamfileout):
+    """Convert sam file to bam file"""
+    cmds = "samtools view -b -S %s > %s"%(samfile, bamfileout)
+    runCommand(cmds, True)
+
+def sortBamFile(bamfile, outSuffix):
+    """docstring for sortBamFile"""
+    cmds = "samtools  sort -m 1000000000  %s  %s" %(bamfile, outSuffix)
+    runCommand(cmds, True)
+
+def index_bam_file(bamfile):
+    """Index bam files"""
+    cmds = "samtools index %s" %(bamfile)
+    runCommand(cmds, True)
+
+def countReadsMappedToProbes(bamindex, outfile):
+    """docstring for countReadsMappedToProbes"""
+    cmds = "samtools idxstats %s > %s " %(bamindex, outfile)
+    runCommand(cmds, True)
+
+def formatCount(input_file, output_file):
+    """Combine the log files from the well splitting routine"""
+    with open(output_file, "w") as oo, open(input_file, "r") as ii:
+        #oo.write("Plate_Barcode\tWell_Barcode\tCount\tLocation\n")
+        for line in ii:
+            if line.startswith("N_"):
+                next
+            else:
+                lines = line.split("\t")
+                newline = lines[0] +"\t" + lines[1] + "\n"
+                oo.write(newline)
+    return
 
 
+def combineCount(input_file_names, output_file):
+    """Combine Count files, merge multiple count files to a single dataframe"""
+    # get string from file name
+    dfs = []
+    for filename in input_file_names:
+        fileN = os.path.basename(filename)
+        records = fileN.split("_")
+        sampleName = records[0] + "_" + records[1] #A11C_S22
+        ids = sampleName
+        count = ids
+        df = pd.read_table(filename, sep= '\t', names=["Genes",count]).set_index("Genes")
+        dfs.append(df)
+    import functools
+    #dfs = [pd.read_table(filename, sep= '\t', names=["gene","len","count","loc"]).set_index("gene") for filename in input_file_names]
+    mergefunc = functools.partial(pd.merge, left_index=True, right_index=True)
+    merged =functools.reduce(mergefunc, dfs)
+    #merged.columns = ['a', 'b']
+    #print merged
+    merged.to_csv(output_file, index=True)
+    return
+
+def alignmentSummary(input_file_name, output_file_name):
+    """Generate alignment summary alignmentSummary"""
+    with open(output_file_name, 'w') as oo, open(input_file_name, 'r') as ii:
+        rec = {}
+        for line in ii:
+            line = line.rstrip()
+            #print line
+            if line.endswith(":") or not line.strip():
+                next
+            else:
+                myList = line.split("\t")
+                key_line =myList[0].strip().split("|")
+                key = key_line[0].strip()
+                val =myList[1].strip()
+                #print key
+                #print val
+                rec[str(key)]= val
+        #print rec
+        numSeq = "Number_of_input_reads\t" + rec['Number of input reads']+"\n"
+        umapped = "Uniquely_mapped_reads_number\t" + rec['Uniquely mapped reads number'] + "\n"
+        umperc = "Uniquely_mapped_reads%\t" + rec['Uniquely mapped reads %'].strip('%') + "\n"
+        ave_map_length = "Average_mapped_length\t" + rec["Average mapped length"] + "\n"
+        multi_map = "Number_of_reads_mapped_to_multiple_loci%\t" + str(float(rec["Number of reads mapped to multiple loci"]) / float(rec['Number of input reads']) * 100) + "\n"
+        unmaped_perc = "Reads_unmapped_too_short%\t" + rec["% of reads unmapped: too short"].strip('%') + "\n"
+        oo.write(numSeq)
+        oo.write(umapped)
+        oo.write(umperc)
+        oo.write(ave_map_length)
+        oo.write(multi_map)
+        oo.write(unmaped_perc)
+
+    return
+
+def combineAlignmentSummary(input_file_names, output_file):
+    """Combine Count files, merge multiple count files to a single dataframe"""
+    # get string from file name
+    dfs = []
+    for filename in input_file_names:
+        fileN = os.path.basename(filename)
+        records = fileN.split("_")
+        sampleName = records[0] + "_" + records[1] #A11C_S22
+        ids = sampleName
+        summary = ids
+        df = pd.read_table(filename, sep= '\t', names=["category",summary]).set_index("category")
+        dfs.append(df)
+
+
+    import functools
+    #dfs = [pd.read_table(filename, sep= '\t', names=["gene","len","count","loc"]).s
+    mergefunc = functools.partial(pd.merge, left_index=True, right_index=True)
+    merged =functools.reduce(mergefunc, dfs)
+    #merged.columns = ['a', 'b']
+    #print merged
+    merged.to_csv(output_file, index=True)
+    return
+
+def plotAlignmentStat(input,output):
+    """plot Alignment summary using ggplot"""
+    df = pd.read_csv(input, thousands=",")
+    # replace % with '' and convert the type to float
+    #df.replace('%', '', regex=True)
+    print df.dtypes
+    # convert to numeric
+    #df1=df.apply(pd.to_numeric, args=('coerce',))
+    # Get certain rows
+    print df
+    df = df.iloc[[2,4,5],]
+    #df = df.ix[['Uniquely mapped reads %', 'Number of reads mapped to multiple loci %', 'Reads unmapped: too short %']]
+    dfm = pd.melt(df, id_vars=['category'], var_name='sampleName',value_name = 'Value')
+
+    print dfm
+    #from ggplot import *
+    #import pandas as pd
+    #df = pd.DataFrame({"x":[1,2,3,4], "y":[1,3,4,2]})
+    #ggplot(aes(x="x", weight="y"), df) + geom_bar()
+    #ggplot(diamonds, aes(x='price', fill='cut')) + geom_histogram() +  theme_bw() + scale_color_brewer(type='qual')
+
+    from ggplot import *
+    p = ggplot(dfm, aes(x='sampleName', weight='Value', fill='category')) + geom_bar(position = "stack") + theme_bw() + ggtitle("Alignment Summary") + scale_y_continuous(labels='comma') + coord_flip()
+    #p = ggplot(df, aes(x = "category", y = "value", fill = "variable")) + \
+        #geom_bar(stat="bar", labels=df["category"].tolist()) + \
+        #theme(axis_text_x = element_text(angle=90))
+    dirname, filename = os.path.split(output)
+    print dirname
+    print filename
+    p.save(output)
+    #ggsave(plot=p, filename=filename, path=dirname)
+    return
+
+
+def filterCombineCount(input, output):
+    """extract count column only"""
+    df = pd.read_csv(input[0])
+    df =df.filter(like='Counts', axis=1)
+    df = df.set_index("Counts")
+    df.to_csv(output, index=True, sep='\t')
+    return
 
 # index genome
 #STAR --runMode genomeGenerate --genomeDir $WRKDIR/star-genome --genomeFastaFiles /path/to/genome/genome.fasta --runThreadN 2
